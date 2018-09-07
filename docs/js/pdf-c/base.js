2e9cecbdd74eba34b9964a9960c49f9456ae7ca1
William Thompson <willinspire@users.noreply.github.com>
2018-09-07 17:30:14 -0400

    Create base.js

---
+++ docs/js/pdf-c/base.js
@@ -0,0 +1,1 @@
+window.onclick=function(b){if(!b.target.matches(".dropdown-menu-button")){var a=document.getElementById("api-menu-dropdown");if(a&&a.classList.contains("dropdown-show")){a.classList.remove("dropdown-show")}}};function jag_dropdown_menu_click(){document.getElementById("api-menu-dropdown").classList.toggle("dropdown-show")};
