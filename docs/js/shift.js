374aea74f4969de69ff5ea41d25b6af68748b9ce
William Thompson <willinspire@users.noreply.github.com>
2018-10-14 18:39:06 -0400

    Update shift.js

--- docs/js/shift.js
+++ docs/js/shift.js
@@ -1,3 +1,5 @@
+document.getElementById("body").style.visibility = "visible";
+
 function bodyShiftA() {
     document.body.innerHTML = "<div class=\"header\"><div class=\"container\"><h3 class=\"display-4\">shift.js<\/h3><p class=\"header-desc\">Switch between HTML pages with pure JavaScript<\/p><\/div><\/div><div class=\"content\"><div class=\"container\"><div class=\"card animated fadeInUp\"><div class=\"card-body\"><div class=\"card-title\"><h3>[ 2 ]<\/h3><\/div><div class=\"card-text\"><hr><p>The secondary HTML page<\/p><button class=\"button btn\" onclick=\"bodyShiftB()\">SHIFT<\/button><\/div><\/div><\/div><\/div><\/div>";
 }
